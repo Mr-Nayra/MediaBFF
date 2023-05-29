f24f4d549ec5f119b75d56e64b309109756b3595
Mr.Nayra <aryanedu5786@gmail.com>
2023-05-29 14:24:46 +0530

    add sidebar functionality

--- src/components/Dashboard/SideBar.js
+++ src/components/Dashboard/SideBar.js
@@ -1,8 +1,11 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const SideBar = () => {
-  
+  const pathname = usePathname();
+
   return (
     <div className="min-w-[20vw] py-10 bg-[#FCFCFC] drop-shadow h-screen sticky top-0">
       <div className="flex items-center justify-around">
@@ -15,22 +18,38 @@ const SideBar = () => {
       </div>
       <div className="mt-[10vh] flex items-center flex-col">
         <Link href="/overview">
-          <p className="text-xl text-[#7C48E8] font-semibold mt-[6vh]">
+          <p
+            className={`${
+              pathname === "/overview" ? "text-[#7C48E8]" : "text-[#656565]"
+            } text-xl font-semibold mt-[6vh]`}
+          >
             OVERVIEW
           </p>
         </Link>
         <Link href="/sessions">
-          <p className="text-xl text-[#7C48E8] font-semibold mt-[6vh]">
+          <p
+            className={`${
+              pathname === "/sessions" ? "text-[#7C48E8]" : "text-[#656565]"
+            } text-xl font-semibold mt-[6vh]`}
+          >
             SESSIONS
           </p>
         </Link>
         <Link href="/latestNews">
-          <p className="text-xl text-[#7C48E8] font-semibold mt-[6vh]">
+          <p
+            className={`${
+              pathname === "/latestNews" ? "text-[#7C48E8]" : "text-[#656565]"
+            } text-xl font-semibold mt-[6vh]`}
+          >
             LATEST NEWS
           </p>
         </Link>
         <Link href="/analytics">
-          <p className="text-xl text-[#7C48E8] font-semibold mt-[6vh]">
+          <p
+            className={`${
+              pathname === "/analytics" ? "text-[#7C48E8]" : "text-[#656565]"
+            } text-xl font-semibold mt-[6vh]`}
+          >
             ANALYTICS
           </p>
         </Link>
